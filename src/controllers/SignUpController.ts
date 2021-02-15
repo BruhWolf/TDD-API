49eeda1fc33b316e95d0d2d39974fb78dd03e444
bruhwolf <b.brunocosta@outlook.com>
2021-02-15 13:39:06 -0300

    feat: ensure SignUpController returns 400 if password and its confirmation is not equal

--- src/controllers/SignUpController.ts
+++ src/controllers/SignUpController.ts
@@ -1,13 +1,18 @@
-import {httpRequest,httpResponse} from '../protocols/http'
+import {HttpRequest,HttpResponse} from '../protocols/http'
+import {Controller} from '../protocols/controller'
 import {badRequest} from '../helpers/badRequest'
 import { MissingFieldsError } from '../errors/MissingFieldsError'
-export class SignUpController {
-  handle (httpRequest: httpRequest): any {
+import { PasswordConfirmationError } from '../errors/PasswordConfirmationError'
+export class SignUpController implements Controller {
+  handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name','email','password','passwordConfirmation']
     for(const field of requiredFields){
       if (!httpRequest.body[field]){
         return badRequest(new MissingFieldsError(field))
       }
+    }
+    if (httpRequest.body.password !== httpRequest.body.passwordConfirmation){
+      return badRequest( new PasswordConfirmationError()
     }
   }
 }
