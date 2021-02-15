7502c8a625b1ad99cdf20ac3e063c97ff162377d
bruhwolf <b.brunocosta@outlook.com>
2021-02-15 15:14:21 -0300

    feat: ensure any server error returns a InternalServerError

--- src/controllers/SignUpController.ts
+++ src/controllers/SignUpController.ts
@@ -1,6 +1,6 @@
 import {HttpRequest,HttpResponse} from '../protocols/http'
 import {Controller} from '../protocols/controller'
-import {badRequest} from '../helpers/badRequest'
+import {badRequest,serverError} from '../helpers/httpResponse.helper'
 import { MissingFieldsError } from '../errors/MissingFieldsError'
 import { PasswordConfirmationError } from '../errors/PasswordConfirmationError'
 import { EmailValidator } from '../protocols/emailValidator'
@@ -10,17 +10,22 @@ import { InvalidFieldsError } from '../errors/InvalidFieldsError'
 export class SignUpController implements Controller {
   constructor(private readonly emailValidator: EmailValidator){}
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name','email','password','passwordConfirmation']
-    for(const field of requiredFields){
-      if (!httpRequest.body[field]){
-        return badRequest(new MissingFieldsError(field))
+    try{
+      const requiredFields = ['name','email','password','passwordConfirmation']
+      for(const field of requiredFields){
+        if (!httpRequest.body[field]){
+          return badRequest(new MissingFieldsError(field))
+        }
       }
-    }
-    if (httpRequest.body.password !== httpRequest.body.passwordConfirmation){
-      return badRequest( new PasswordConfirmationError())
-    }
-    if(!this.emailValidator.isValid(httpRequest.body.email)){
-      return badRequest( new InvalidFieldsError('email'))
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation){
+        return badRequest( new PasswordConfirmationError())
+      }
+      if(!this.emailValidator.isValid(httpRequest.body.email)){
+        return badRequest( new InvalidFieldsError('email'))
+      }
+    }catch(err){
+      console.error(err)
+      return serverError()
     }
   }
 }
