6fbb2e9bd1565f02cc1859e8766addaa986fa290
bruhwolf <b.brunocosta@outlook.com>
2021-02-15 12:31:44 -0300

    feat: simplify code

--- src/controllers/SignUpController.ts
+++ src/controllers/SignUpController.ts
@@ -2,18 +2,16 @@ import {httpRequest,httpResponse} from '../protocols/http'
 import {badRequest} from '../helpers/badRequest'
 import { MissingFieldsError } from '../errors/MissingFieldsError'
 export class SignUpController {
-
   handle (httpRequest: httpRequest): httpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingFieldsError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingFieldsError('email'))
-    }
+    const requiredFields = ['name','email','password','passwordConfirmation']
+    requiredFields.map( field => {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingFieldsError(field))
+      }
+    })
     return{
       statusCode: 200,
       body: 'bem vindo'
     }
-
   }
 }
