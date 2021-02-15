2cca97c3c69f4bc60c41e4b26ccbfc878c3ea825
bruhwolf <b.brunocosta@outlook.com>
2021-02-15 20:07:28 -0300

    refactor: rename sut and add emailValidation mock

--- src/controllers/SignUpController.spec.ts
+++ src/controllers/SignUpController.spec.ts
@@ -2,30 +2,11 @@ import { InvalidFieldsError, MissingFieldsError, PasswordConfirmationError, Inte
 import { EmailValidator } from '../protocols/emailValidator'
 import { SignUpController } from './SignUpController'
 
-interface SutTypes{
-  sut: SignUpController
-  emailValidatorStub: EmailValidator
-}
-const makeSut = (): SutTypes => {
 
-  class EmailValidatorStub implements EmailValidator{
-    isValid(email:string){
-      return true
-    }
+class EmailValidatorStub implements EmailValidator{
+  isValid(email:string){
+    return true
   }
-  const emailValidatorStub = new EmailValidatorStub()
-  const sut = new SignUpController(emailValidatorStub)
-  return{
-    sut,
-    emailValidatorStub
-   }
-
-}
-interface AccountDTO {
-  name?: string
-  email?: string
-  password?: string
-  passwordConfirmation?: string
 }
 class AccountBuilder {
     public name?:string ='teste'
@@ -68,89 +49,70 @@ class AccountBuilder {
   }
 }
 
+const emailValidatorStub =  new EmailValidatorStub()
+const signUpController = new SignUpController( emailValidatorStub)
+
 
 describe('SignUpController', () => {
 })
 
 test('should return 400 if no name is provided', () => {
-  const {sut} = makeSut()
-  const httpRequest = {
-    body: AccountBuilder.anAccount().withNoName()
-  }
-  const httpResponse = sut.handle(httpRequest)
+  const httpRequest = {  body: AccountBuilder.anAccount().withNoName()  }
+  const httpResponse = signUpController.handle(httpRequest)
   expect(httpResponse.statusCode).toBe(400)
   expect(httpResponse.body).toEqual(new MissingFieldsError('name'))
 })
 
 test('should return 400 if no email is provided', () => {
-  const {sut} = makeSut()
-  const httpRequest = {
-    body: AccountBuilder.anAccount().withNoEmail()
-  }
-  const httpResponse = sut.handle(httpRequest)
+  const httpRequest = { body: AccountBuilder.anAccount().withNoEmail() }
+  const httpResponse = signUpController.handle(httpRequest)
   expect(httpResponse.statusCode).toBe(400)
   expect(httpResponse.body).toEqual(new MissingFieldsError('email'))
 })
 
 test('should return 400 if no password is provided', () => {
-  const {sut} = makeSut()
-  const httpRequest = {
-    body: AccountBuilder.anAccount().withNoPassword()
-  }
-  const httpResponse = sut.handle(httpRequest)
+  const httpRequest = {  body: AccountBuilder.anAccount().withNoPassword() }
+  const httpResponse = signUpController.handle(httpRequest)
   expect(httpResponse.statusCode).toBe(400)
   expect(httpResponse.body).toEqual(new MissingFieldsError('password'))
 })
 
 test('should return 400 if no passwordConfirmation is provided', () => {
-  const {sut} = makeSut()
-  const httpRequest = {
-    body: AccountBuilder.anAccount().withNoPasswordConfirmation()
-  }
-  const httpResponse = sut.handle(httpRequest)
+  const httpRequest = { body: AccountBuilder.anAccount().withNoPasswordConfirmation() }
+  const httpResponse = signUpController.handle(httpRequest)
   expect(httpResponse.statusCode).toBe(400)
   expect(httpResponse.body).toEqual(new MissingFieldsError('passwordConfirmation'))
 })
 
-test('should return 400 if password is not equal to passwordConfirmation', () => {
-  const {sut} = makeSut()
-  const httpRequest = {
-    body: AccountBuilder.anAccount().withWrongPasswordConfirmation()
-  }
-  const httpResponse = sut.handle(httpRequest)
+test('should return 400 if password confirmation fails', () => {
+  const httpRequest = { body: AccountBuilder.anAccount().withWrongPasswordConfirmation() }
+  const httpResponse = signUpController.handle(httpRequest)
   expect(httpResponse.statusCode).toBe(400)
   expect(httpResponse.body).toEqual(new PasswordConfirmationError())
 })
 
 test('should return 400 if a invalid email is provided', () => {
-  const {sut, emailValidatorStub} = makeSut()
   jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
   const httpRequest = {
     body: AccountBuilder.anAccount().withInvalidEmail()
   }
-  const httpResponse = sut.handle(httpRequest)
+  const httpResponse = signUpController.handle(httpRequest)
   expect(httpResponse.statusCode).toBe(400)
   expect(httpResponse.body).toEqual(new InvalidFieldsError('email'))
 })
 
 test('should call emailValidator with correct email', () => {
-  const {sut, emailValidatorStub} = makeSut()
   const isValid = jest.spyOn(emailValidatorStub, 'isValid')
   const body = AccountBuilder.anAccount()
-  const httpRequest = {
-    body: body
-  }
-  sut.handle(httpRequest)
+  const httpRequest = { body: body  }
+  signUpController.handle(httpRequest)
   expect(isValid).toBeCalledWith(body.email)
 })
 
 test('should return 500 if EmailValidator throws any error', () => {
-  const {sut, emailValidatorStub} = makeSut()
-  const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
-  const httpRequest = {
-    body: AccountBuilder.anAccount()
-  }
-  const httpResponse = sut.handle(httpRequest)
+  jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(()=>{throw new Error('Unknow error')})
+  const httpRequest = { body: AccountBuilder.anAccount()  }
+  const httpResponse = signUpController.handle(httpRequest)
   expect(httpResponse.statusCode).toBe(500)
   expect(httpResponse.body).toEqual(new InternalServerError())
 })
