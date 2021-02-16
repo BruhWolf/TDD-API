41f3203ed164b27f8b5c15b0b0017aa44736637c
bruhwolf <b.brunocosta@outlook.com>
2021-02-16 16:51:18 -0300

    test: emailvalidador adapter

--- src/utils/EmailValidatorAdapter.spec.ts
+++ src/utils/EmailValidatorAdapter.spec.ts
@@ -1,30 +1,28 @@
-import { EmailValidator } from "../presentation/protocols/emailValidator"
-import {EmailValidatorAdapter,validator} from './EmailVAlidatorAdapter'
-
+import {EmailValidatorAdapter,emailSchema} from './EmailVAlidatorAdapter'
 
 const emailVAlidationAdapterStub = new EmailValidatorAdapter()
 describe('Email VAlidator Adapter',  () => {
 
   test('should return false if EmailValidatorAdapter returns false', async () => {
-    jest.spyOn(emailVAlidationAdapterStub,'isEmail').mockImplementationOnce( async () => {
+    jest.spyOn(emailVAlidationAdapterStub,'isValid').mockImplementationOnce( async () => {
         return new Promise(resolve=>resolve(false))
     })
-    const validatorResponse = await emailVAlidationAdapterStub.isEmail('invalidEmail@test.com')
+    const validatorResponse = await emailVAlidationAdapterStub.isValid('invalidEmail@test.com')
     expect(validatorResponse).toBe(false)
   }) 
 
   test('should return true if EmailValidatorAdapter returns true', async () => {
-    jest.spyOn(emailVAlidationAdapterStub,'isEmail').mockImplementationOnce( async () => {
+    jest.spyOn(emailVAlidationAdapterStub,'isValid').mockImplementationOnce( async () => {
         return new Promise(resolve=>resolve(true))
     })
-    const validatorResponse = await emailVAlidationAdapterStub.isEmail('validEmail')
+    const validatorResponse = await emailVAlidationAdapterStub.isValid('validEmail')
     expect(validatorResponse).toBe(true)
   })
   
   test('should call EmailValidatorAdapter with correct email', async () => {
       const email = 'test@teste.com'
-      const spy = jest.spyOn(validator,'validate')
-      await emailVAlidationAdapterStub.isEmail(email)
+      const spy = jest.spyOn(emailSchema,'validate')
+      await emailVAlidationAdapterStub.isValid(email)
       expect(spy).toHaveBeenCalledWith(email)
   })
 })
